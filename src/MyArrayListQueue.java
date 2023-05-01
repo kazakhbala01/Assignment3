d477269d7d0117e524c310b764038741736da088
Almas <amnbyalms@gmail.com>
2023-05-01 16:18:02 +0600

    2

---
+++ src/MyArrayListQueue.java
@@ -0,0 +1,7 @@
+import java.util.NoSuchElementException;
+public class MyArrayListQueue<T> {
+    private MyArrayList<T> queue;
+    public MyArrayListQueue(){
+        queue=new MyArrayList<>();
+    }
+}
