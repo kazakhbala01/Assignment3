1178337fbb74460d850e5e9de35a40d8b7e39165
Almas <amnbyalms@gmail.com>
2023-05-01 16:30:21 +0600

    3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        MyArrayListQueue<Integer> que=new MyArrayListQueue<>();
+        que.enqueue(5);
+        que.size();
+        que.peek();
+        MyArrayListStack<Integer> stck = new MyArrayListStack<>();
+        stck.push(5);
+        stck.getSize();
     }
 }
